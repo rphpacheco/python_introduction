26ca7a4d1237f0e385b296123ae7103d09c3485c
rphpacheco <rphpacheco@gmail.com>
2021-12-20 09:35:57 -0300

    second part of meetings

--- 08 - 02 challenge.py
+++ 08 - 02 challenge.py
@@ -8,3 +8,21 @@
     Mas se a pessoa for maior de 60, os juros serão de 4.22
     E no final, o sistema deverá exibir o valor total que a pessoa irá pagar com juros
 """
+
+idade = int(input("Digite sua idade: "))
+
+if idade < 18:
+    print("Você não pode pegar empréstimo.")
+    exit()
+
+valor = float(input("Digite o valor do empréstimo: "))
+
+if idade < 31:
+    juros = 2.09
+elif idade < 61:
+    juros = 3.87
+else:
+    juros = 4.22
+
+print("O valor dos juros é de: {:.2f}%".format(juros))
+print("O valor total a pagar é: R$ %.2f" % (valor + (valor * juros / 100)))
